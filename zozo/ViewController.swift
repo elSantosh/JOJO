553024c4e83a727164515dcabfe8b6e366dfecda
Santosh <santosh@workstreak.me>
2018-04-20 10:36:30 +0800

    Initial Commit

---
+++ zozo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zozo
+//
+//  Created by Santosh Guruju | MACROKIOSK on 20/04/18.
+//  Copyright © 2018 workstreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
